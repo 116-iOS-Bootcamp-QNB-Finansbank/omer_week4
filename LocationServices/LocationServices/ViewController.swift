96bb7140d53faca70ec6f401acfe4c78e21c4fcc
Ömer Köse <omer.kose@ibtech.com.tr>
2021-09-29 22:21:37 +0300

    Added background location update

--- LocationServices/LocationServices/ViewController.swift
+++ LocationServices/LocationServices/ViewController.swift
@@ -21,10 +21,16 @@ class ViewController: UIViewController {
     private func initalizeLocationServices() {
         locationServices.delegate = self
         
+        /* Allow application to update location in the background */
+        locationServices.allowsBackgroundLocationUpdates = true
+        /* Notify the user about the usage of the location via the background location indicator*/
+        locationServices.showsBackgroundLocationIndicator = true
+        
         guard CLLocationManager.locationServicesEnabled() else {
             return
         }
         
+        /* Request the location from the user */
         locationServices.requestAlwaysAuthorization()
     }
 }
@@ -33,17 +39,27 @@ extension ViewController: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         let status = manager.authorizationStatus
         
+        /* Cases above is to display or execute specific actions for the application depending on
+         the location permission given by the user, also display the current permission status as an output
+         in the console */
         switch status {
         case .notDetermined:
             print("notDetermined")
+            locationServicesNeededState()
         case .denied:
             print("denied")
+            promptForAuthorization()
         case .restricted:
             print("restricted")
+            locationServicesRestrictedState()
         case .authorizedAlways:
             print("authorizedAlways")
+            /* Start updating location of the user when given permission */
+            locationServices.startUpdatingLocation()
         case .authorizedWhenInUse:
             print("authorizedWhenInUse")
+            /* Start updating location of the user when given permission */
+            locationServices.startUpdatingLocation()
         default:
             print("unknown")
         }
@@ -70,6 +86,10 @@ extension ViewController {
         ])
     }
     
+    /* This function directs user to the settings when location permission is not given
+     an alert box is being created for the user to interact
+     first option to direct user to the settings of the device
+     second is to cancel and terminate the permission process */
     func promptForAuthorization() {
         let alert = UIAlertController(title: "Location access is needed to get your current location", message: "Please allow access", preferredStyle: .alert)
         let settingsAction = UIAlertAction(title: "Settings", style: .default, handler: {
@@ -86,10 +106,12 @@ extension ViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    /* This function displays the current status of the permission given by the user (DENIED) */
     func locationServicesNeededState() {
-        self.statusLabel.text = "Access to locaiton services is needed."
+        self.statusLabel.text = "Access to location services is needed."
     }
     
+    /* This function displays the current status of the permission given by the user (RESTRICTED) */
     func locationServicesRestrictedState() {
         self.statusLabel.text = "The app is restricted from using the location servies."
     }
